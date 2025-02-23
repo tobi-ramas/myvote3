d3d41087f870e92b108eba9bb0e9fab1da5efe8e
tobi-ramas <tobiramas12345@gmail.com>
2025-02-24 03:57:23 +0530

    Update click_button.py

--- click_button.py
+++ click_button.py
@@ -18,10 +18,7 @@ user_data_dir = f"/tmp/chrome-user-data-{uuid.uuid4()}"
 options.add_argument(f'--user-data-dir={user_data_dir}')
 
 # Set the User-Agent to mimic a mobile device (e.g., Chrome on Android)
-mobile_user_agent = (
-    "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36"
-)
+mobile_user_agent = "Mozilla/5.0 (Linux; Android 11; ONEPLUS A2023) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36"
 options.add_argument(f'user-agent={mobile_user_agent}')
 
 # Set mobile emulation options
